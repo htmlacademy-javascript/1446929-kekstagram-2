311a7310c2b0f2736bfb797120b81c6d9bd2d850
Natalya Kim <kim87324@yandex.ru>
2025-06-04 10:19:12 +0300

    add photo form upload validation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import { addPhotos } from './data.js';
 import { renderPhotos } from './photos.js';
+import { initPhotoUploadForm } from './upload-photo.js';
 
 renderPhotos(addPhotos());
+
+initPhotoUploadForm();
