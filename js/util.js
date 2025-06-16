ad65ba029b2392261626d9b8f0cf23007cd2f741
Natalya Kim <kim87324@yandex.ru>
2025-06-16 12:03:42 +0300

    add fetch api

--- js/util.js
+++ js/util.js
@@ -1,7 +1,3 @@
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-const getRandomArrayItem = (items) => items[getRandomNumber(0, items.length - 1)];
-
 const toggleClass = (element, className = '') => {
   if (element) {
     element.classList.toggle(className);
@@ -11,4 +7,4 @@ const toggleClass = (element, className = '') => {
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 
-export { getRandomNumber, getRandomArrayItem, toggleClass, isEscapeKey };
+export { toggleClass, isEscapeKey };
